7245bfdf5fa1283a6f5a6521bb748e5311828b24
Vijay Dev <vijaydev.in@gmail.com>
2023-05-06 09:23:36 +0530

    [MINOR] Initialize a Qwik website (#1)

---
+++ changelog.md
@@ -0,0 +1,2 @@
+# [vNext] (release 2023-)
+- [#1](https://github.com/iamvijaydev/vijaydev.com/issues/1) [MINOR] Initialize a Qwik website
---
+++ src/routes/blog/index.tsx
@@ -0,0 +1,20 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Blog listing will go here</h1>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};
