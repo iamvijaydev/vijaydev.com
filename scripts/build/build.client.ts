f4a023f35260a9155c0483a0c5f5ea0ba86f500c
Vijay Dev <vijaydev.official@gmail.com>
2024-06-20 18:14:29 +0530

    [#1004] Create script to build client and server js files

---
+++ scripts/build/build.client.ts
@@ -0,0 +1,94 @@
+import * as esbuild from "esbuild";
+import mdx from "@mdx-js/esbuild";
+
+import { AppProps } from "../types";
+import { baseClientBuildConfig, mdxPlugins } from "./build.config";
+import {
+  getRoutes,
+  getImports,
+  getBlogData,
+  getLearnData,
+} from "../store/store";
+import { getMainTemplate } from "../templates/getMainTemplate";
+import { getContentItemTemplate } from "../templates/getContentItemTemplate";
+import { getContentListingTemplate } from "../templates/getContentListingTemplate";
+
+export const buildFiles = async () => {
+  console.time("client mjs files built");
+
+  const appData: AppProps = {
+    imports: getImports(),
+    blogs: getBlogData(),
+    learn: getLearnData(),
+  };
+  function replacer(key: string, value: unknown) {
+    if (value instanceof Map) {
+      return {
+        dataType: "Map",
+        value: Array.from(value.entries()),
+      };
+    } else {
+      return value;
+    }
+  }
+  const asStr = JSON.stringify(appData, replacer);
+
+  await esbuild.build({
+    ...baseClientBuildConfig,
+    stdin: {
+      contents: getMainTemplate(asStr),
+      resolveDir: "./src",
+      loader: "tsx",
+    },
+    outfile: "dist/client/assets/main.mjs",
+  });
+
+  for await (const route of getRoutes().values()) {
+    if (!route.input.template) {
+      await esbuild.build({
+        ...baseClientBuildConfig,
+        entryPoints: [route.input.source],
+        entryNames: "[name]",
+        outfile: route.output.mjs,
+      });
+      continue;
+    }
+
+    if (route.input.source === "" && route.input.template) {
+      await esbuild.build({
+        ...baseClientBuildConfig,
+        stdin: {
+          contents: getContentListingTemplate({
+            template: route.input.template,
+            title: route.matter!.title,
+            description: route.matter!.description,
+            pathname: route.route.pathname,
+          }),
+          resolveDir: "./src",
+          loader: "tsx",
+        },
+        outfile: route.output.mjs,
+      });
+      continue;
+    }
+
+    await esbuild.build({
+      ...baseClientBuildConfig,
+      stdin: {
+        contents: getContentItemTemplate({
+          source: route.input.source,
+          template: route.input.template,
+          title: route.matter!.title,
+          pathname: route.route.pathname,
+          parentPathname: route.route.parentPathname!,
+        }),
+        resolveDir: "./src",
+        loader: "tsx",
+      },
+      outfile: route.output.mjs,
+      plugins: [mdx(mdxPlugins)],
+    });
+  }
+
+  console.timeEnd("client mjs files built");
+};
